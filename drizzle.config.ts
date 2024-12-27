a89b4838a20efc5bfaff853d53409806244073cb
Natanael <taelima1997@gmail.com>
2024-12-27 10:59:41 -0300

    Refactor database configuration in drizzle.config.ts
    - Changed the database driver from 'pg' to 'postgresql' for improved clarity and consistency in configuration.
    - Added SSL option to dbCredentials, enhancing security for database connections.
    - Updated the configuration structure to better align with best practices for database management.
    
    These changes improve the clarity and security of the database configuration, setting a solid foundation for future development.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,12 +6,13 @@ dotenv.config();
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     user: process.env.DB_USER || 'admin',
     password: process.env.DB_PASSWORD || 'admin123',
     database: process.env.DB_NAME || 'orders_db',
+    ssl: false
   },
 } satisfies Config; 
